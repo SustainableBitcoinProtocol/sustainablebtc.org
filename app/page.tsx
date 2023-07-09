a7b306a3a4ae520bf38829575e8e0a0026ddafa1
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2023-07-09 09:33:57 +0530

    fonts added, tailwind configured

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import { getNavbar } from "@/sanity/sanity-utils";
+import styles from "@/styles/pages/Home.module.scss";
 
 export default async function Home() {
    const navbar = await getNavbar();
    return (
-      <div>
-         Navbar
-         {navbar.map((navItem) => (
+      <div className={styles.bgRed}>
+         <span className="text-3xl font-black tracking-tighter">Main</span>
+         {/* {navbar.map((navItem) => (
             <>
                <div>
                   {navItem.name}/{navItem.slug}
                </div>
             </>
-         ))}
+         ))} */}
       </div>
    );
 }
