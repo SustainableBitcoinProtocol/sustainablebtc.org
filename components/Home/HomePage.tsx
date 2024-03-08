f5e3685872f2a4dc5543132593d7f47398e62788
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2024-03-08 09:59:17 +0530

    miners added on home page

--- components/Home/HomePage.tsx
+++ components/Home/HomePage.tsx
@@ -2,17 +2,20 @@ import React from "react";
 
 // import components
 import HomeHero from "./HomeHero";
+import HomeMiners from "./HomeMiners";
 import HomeSBC from "./HomeSBC";
 import HomeTestimonials from "./HomeTestimonials";
 import HomeTrust from "./HomeTrust";
 
 // import Sanity
-import { getHomePageData } from "@/sanity/sanity-utils";
+import { getHomePageData, getMinerPageData } from "@/sanity/sanity-utils";
 
 export default async function HomePage() {
    // Get Data
    const homePageData = await getHomePageData();
+   const minerPageData = await getMinerPageData();
    const heroData = homePageData.hero || null;
+   const minerData = minerPageData.hero || null;
    const sbcData = homePageData.sbc || null;
    const testimonialData = homePageData.testimonials || null;
    const trustData = homePageData.trust || null;
@@ -20,6 +23,7 @@ export default async function HomePage() {
    return (
       <>
          <HomeHero heroData={heroData} />
+         <HomeMiners minerData={minerData}/>
          <HomeSBC sbcData={sbcData} />
          <HomeTestimonials testimonialData={testimonialData} />
          <HomeTrust trustData={trustData} />
