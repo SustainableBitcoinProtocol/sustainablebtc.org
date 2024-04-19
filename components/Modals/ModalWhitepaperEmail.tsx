5c06d6afd0a2a054b5ef9b01b29a614a9d480e7c
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2024-04-19 09:03:16 +0530

    modal design updated on dev

--- components/Modals/ModalWhitepaperEmail.tsx
+++ components/Modals/ModalWhitepaperEmail.tsx
@@ -3,9 +3,14 @@
 // Importing Libraries
 import React, { useEffect } from "react";
 import Modal from "react-modal";
+import Image from "next/image";
 
 // Importing styles
 import styles from "@/styles/components/Modal.module.scss";
+
+// Importing Images
+import imgModalBackdrop from "@/public/home/modalWhitepaperBackdrop.png";
+import imgModalForeground from "@/public/home/modalWhitepaperForeground.png";
 
 const customStyles = {
    content: {
@@ -14,8 +19,8 @@ const customStyles = {
       right: "auto",
       bottom: "auto",
       transform: "translate(-50%, -50%)",
-      color: "#1e203f",
-      maxWidth: "600px",
+      color: "#fafafa",
+      maxWidth: "800px",
       width: "calc(100% - 100px)",
       padding: "2rem",
       borderRadius: "0",
@@ -44,42 +49,62 @@ const ModalWhitepaperEmail = ({
             style={customStyles}
             contentLabel="Example Modal"
          >
-            <div className={`${styles.modal}`}>
-               {/* Modal Header */}
-               <div className={`${styles.modalHeader}`}>
-                  <h3
-                     className={`${styles.modalHeader__heading} heading heading-4`}
-                  >
-                     Download Whitepaper
+            <div className={`${styles.modal} ${styles.modalWhitepaperEmail}`}>
+               {/* Actual Content */}
+               <div className={`${styles.content}`}>
+                  <h3 className={`${styles.heading} heading heading-4`}>
+                     Download SBP Whitepaper
                   </h3>
-                  <button onClick={closeModal}>
-                     <i className="bi bi-x-lg"></i>
-                  </button>
+                  <p className={`${styles.para} para`}>
+                     Thankyou for showing interest in SBP. <br />
+                     Our whitepaper is available for free to download.
+                  </p>
+                  <form action="#" className={`${styles.form}`}>
+                     <div className={`${styles.formGroup} form-group`}>
+                        <label htmlFor="email">
+                           Email <span>*</span>
+                        </label>
+                        <input
+                           type="text"
+                           className="form-control"
+                           placeholder="Enter your email"
+                           name="email"
+                        />
+                        <span className="error"></span>
+                     </div>
+                     <button className={`${styles.formBtn} btn btn-dark`}>
+                        <i className="bi bi-download"></i>
+                        <span>Download Now</span>
+                     </button>
+                  </form>
                </div>
 
-               {/* Modal Body */}
-               <div className={`${styles.modalBody}`}>
-                  <p>
-                     {`Thankyou for showing interest in our whitepaper. It's free to download.`}
-                  </p>
-                  <div className={`${styles.formGroup} form-group`}>
-                     <label htmlFor="email">
-                        Enter email <span>*</span>
-                     </label>
-                     <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Email *"
-                        name="email"
-                     />
-                     <span className="error"></span>
-                  </div>
-                  <div className={`${styles.btnWrapper}`}>
-                     <button className={`${styles.btn} btn btn-primary`}>
-                        <i className="bi bi-download"></i>
-                        <span>Download Whitepaper</span>
-                     </button>
-                  </div>
+               {/* Design Elements */}
+
+               <div className={`${styles.designElem}`}>
+                  {/* Close Button */}
+                  <button
+                     className={`${styles.designElem__closeBtn}`}
+                     onClick={closeModal}
+                  >
+                     <i className="bi bi-x-lg"></i>
+                  </button>
+
+                  {/* Backdrop Image */}
+                  <Image
+                     src={imgModalBackdrop}
+                     alt="Hero Background"
+                     className={styles.imgModalBackdrop}
+                     objectFit="contain"
+                  />
+
+                  {/* Foredrop Image */}
+                  <Image
+                     src={imgModalForeground}
+                     alt="Hero Background"
+                     className={styles.imgModalForeground}
+                     objectFit="contain"
+                  />
                </div>
             </div>
          </Modal>
