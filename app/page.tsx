063c7662c6f22ef1976e2636e518b79b50886a03
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2024-09-17 22:47:00 +0530

    hotfix

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import HomePage from "@/components/HomeNew/HomePage";
 export const metadata: Metadata = {
    title: "Sustainable Bitcoin Protocol | BTC Energy Consumption Solution",
    description:
-      "Sustainable Bitcoin Protocol helps accelerate the bitcoin network's transition to clean energy.",
+      "Sustainable Bitcoin Protocol (SBP), is the leading solution to align Bitcoin investing with the clean energy transition. SBP introduces Sustainable Bitcoin Certificates (SBCs) which transform a Bitcoin portfolio into a climate-positive asset by ensuring bitcoin mining supports clean energy and methane mitigation. With unparalleled transparency and valuable insights into energy consumption, SBCs provide a verifiable way for institutional investors to drive environmental impact while investing in Bitcoin. Invest in Bitcoin with confidence, knowing your portfolio not only delivers returns but also drives meaningful climate action.",
 };
 
 export default function Home() {
