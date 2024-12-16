d7267804bc58213d9f688953c60b3dd6e4043c75
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2024-12-16 13:23:55 +0530

    Clickjacking Vulnerability FIX

--- next.config.js
+++ next.config.js
@@ -7,7 +7,20 @@ const nextConfig = {
    },
    images: {
       domains: ['cdn.sanity.io', 'lcw.nyc3.cdn.digitaloceanspaces.com']
-   }
+   },
+
+   // Fix for click jacking vulnerability
+   async headers() {
+      return [
+         {
+            source: '/(.*)', // Apply to all routes
+            headers: [
+               { key: 'X-Frame-Options', value: 'DENY' },
+               { key: 'Content-Security-Policy', value: "frame-ancestors 'none';" }
+            ],
+         },
+      ];
+   },
 }
 
 module.exports = nextConfig
