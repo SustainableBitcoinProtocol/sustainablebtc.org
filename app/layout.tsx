dd807ed0fa1ae0f04a24f691c87b2dbfe2ab3265
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2024-12-17 11:09:00 +0530

    ROLL BACK

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,19 +4,67 @@ import "@/styles/global.scss";
 // Imports
 import localFont from "@next/font/local";
 import Script from "next/script";
-import ConditionalLayout from "@/components/ConditionalLayout";
 
-// Importing fonts
+// Custom Components
+import Header from "@/components/HeaderFooter/Header";
+import Footer from "@/components/Footer";
+import Breadcrum from "@/components/Breadcrum";
+import ProgressBar from "@/components/ProgressBar";
+
+// Importing icons
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/free-mode";
+
+// Defining Fonts
 const helveticaNowDisplay = localFont({
    src: [
-      { path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-Thin.ttf", weight: "100", style: "normal" },
-      { path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-Regular.ttf", weight: "400", style: "normal" },
-      { path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-Bold.ttf", weight: "700", style: "normal" },
+      {
+         path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-Thin.ttf",
+         weight: "100",
+         style: "normal",
+      },
+      {
+         path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-ExtLt.ttf",
+         weight: "200",
+         style: "normal",
+      },
+      {
+         path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-Light.ttf",
+         weight: "300",
+         style: "normal",
+      },
+      {
+         path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-Regular.ttf",
+         weight: "400",
+         style: "normal",
+      },
+      {
+         path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-Medium.ttf",
+         weight: "500",
+         style: "normal",
+      },
+      {
+         path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-Bold.ttf",
+         weight: "700",
+         style: "normal",
+      },
+      {
+         path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-ExtraBold.ttf",
+         weight: "800",
+         style: "normal",
+      },
+      {
+         path: "../public/fonts/helvetica-now-display/HelveticaNowDisplay-Black.ttf",
+         weight: "900",
+         style: "normal",
+      },
    ],
 });
 
 export default function RootLayout({
-   children,
+   children, // will be a page or nested layout
 }: {
    children: React.ReactNode;
 }) {
@@ -38,8 +86,16 @@ export default function RootLayout({
          </>
          <html lang="en" className={helveticaNowDisplay.className}>
             <body>
-               {/* Conditionally Render Layout */}
-               <ConditionalLayout>{children}</ConditionalLayout>
+               {/* Progressbar */}
+               <ProgressBar />
+               {/* Navbar */}
+               <Header />
+               {/* Breadcrum */}
+               <Breadcrum />
+               {/* Main */}
+               <main>{children}</main>
+               {/* Footer */}
+               <Footer />
             </body>
          </html>
       </>
