b44ae7ad5b2d2fc547c0f9a279a7c81c15fe6f0f
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2025-02-16 18:51:18 +0530

    learning centre video popup

--- components/LearningCentre/LearningCentreLesson.tsx
+++ components/LearningCentre/LearningCentreLesson.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import Link from "next/link";
 import Image from "next/image";
 import { urlFor } from '@/sanity/sanity-urlFor';
+import ModalVideo from "react-modal-video";
 
 // Styles
 import styles from "@/styles/pages/LearningCentre.module.scss";
+import "react-modal-video/css/modal-video.css";
 import { PortableText } from '@portabletext/react';
 
 const LearningCentreLesson = ({ videoLessonData }: { videoLessonData: any }) => {
+   const [isOpen, setIsOpen] = useState(false);
+   const [videoId, setVideoId] = useState("");
+
+   function openVideoModal(videoId: any){
+      setVideoId(videoId);
+      setIsOpen(true);
+   }
+
    return (
       <>
          <section className={styles.videoLesson}>
@@ -20,7 +32,7 @@ const LearningCentreLesson = ({ videoLessonData }: { videoLessonData: any }) => 
                               <div className={styles.videoItem} key={i}>
                                  {/* Image */}
                                  <div className={styles.videoItemImageWrapper}>
-                                    <span className={styles.videoItemImageBubble}>Lesson {i+1}</span>
+                                    <span className={styles.videoItemImageBubble}>Lesson {i + 1}</span>
                                     {lesson.videoLessonImage && (
                                        <Image
                                           src={urlFor(lesson.videoLessonImage)
@@ -62,18 +74,22 @@ const LearningCentreLesson = ({ videoLessonData }: { videoLessonData: any }) => 
                                        {lesson.videoLessonButton.map((cta: any, i: number) => (
                                           <>
                                              {cta.btnOptions.btnVisible && (
-                                                <Link
-                                                   key={i}
-                                                   href={cta.btnOptions.btnSlug}
-                                                   className={`btn btn-${cta.btnOptions.btnType}`}
-                                                >
-                                                   <span>{cta.btnText}</span>
-                                                   {cta.btnOptions.btnIcon !== "NA" && (
-                                                      <i
-                                                         className={`bi bi-${cta.btnOptions.btnIcon}`}
-                                                      ></i>
-                                                   )}
-                                                </Link>
+                                                <>
+                                                   {/* Button */}
+                                                   <button
+                                                      key={i}
+                                                      // href={cta.btnOptions.btnSlug}
+                                                      className={`btn btn-${cta.btnOptions.btnType}`}
+                                                      onClick={() => openVideoModal(cta.btnOptions.btnSlug)}
+                                                   >
+                                                      <span>{cta.btnText}</span>
+                                                      {cta.btnOptions.btnIcon !== "NA" && (
+                                                         <i
+                                                            className={`bi bi-${cta.btnOptions.btnIcon}`}
+                                                         ></i>
+                                                      )}
+                                                   </button>
+                                                </>
                                              )}
                                           </>
                                        ))}
@@ -86,6 +102,14 @@ const LearningCentreLesson = ({ videoLessonData }: { videoLessonData: any }) => 
                   </>
                }
             </div>
+
+            {/* Modal Video */}
+            <ModalVideo
+               channel="youtube"
+               isOpen={isOpen}
+               videoId={videoId}
+               onClose={() => setIsOpen(false)}
+            />
          </section>
       </>
    )
